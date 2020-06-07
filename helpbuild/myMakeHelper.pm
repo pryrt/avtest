4b748e1c8232b3732301a032ceb0f3c322eb6c71
PeterCJ <pryrtcode@pryrt.com>
2020-06-07 14:34:55 -0700

    added in the helpbuild\myMakeHelper, which was missing in last commit; should get through downloading zipfile now

---
+++ helpbuild/myMakeHelper.pm
@@ -0,0 +1,137 @@
+package myMakeHelper;
+use warnings;
+use strict;
+use 5.010;
+use Exporter 5.57 qw(import);
+use File::Which ();
+use File::Spec ();
+use File::Fetch;
+use Config;
+
+our %EXPORT_TAGS = (
+    default => [qw/myMakeHelper/],
+);
+our @EXPORT = qw(myMakeHelper);
+our @EXPORT_OK = (@EXPORT, map {@$_} values %EXPORT_TAGS);
+
+sub myMakeHelper {
+    my %ret = ();
+
+    warn __PACKAGE__, "\tAUTOMATED_TESTING = ", $ENV{AUTOMATED_TESTING}//'<undef>', "\n";
+    warn __PACKAGE__, "\tTEMP = ", $ENV{TEMP}//'<undef>', "\n";
+    warn __PACKAGE__, "\tTMP = ", $ENV{TMP}//'<undef>', "\n";
+
+    is_windows() or return %ret;   # TODO: eventually add AUTOMATED_TESTING check as well
+
+    npp_already_exists() and return %ret;
+
+    my $bits = determine_bitness() or return %ret;
+    $ret{bits} = $bits;
+
+    my $zip = download_zip( $bits, File::Spec->tmpdir ) or return %ret;
+    $ret{zip} = $zip;
+
+    return %ret;
+}
+
+sub is_windows { $^O eq 'MSWin32' or $^O eq 'cygwin' or $ENV{AVTEST_FORCE_NON_WIN} }
+
+sub PRETEND_IT_DOESNT { 1; } # set to 0
+
+sub npp_already_exists {
+    my $npp_exe;
+    # priority to path, 64bit, default, then x86-specific locations
+    my @try = ( File::Which::which('notepad++') );
+    push @try, "$ENV{ProgramW6432}/Notepad++/notepad++.exe" if exists $ENV{ProgramW6432};
+    push @try, "$ENV{ProgramFiles}/Notepad++/notepad++.exe" if exists $ENV{ProgramFiles};
+    push @try, "$ENV{'ProgramFiles(x86)'}/Notepad++/notepad++.exe" if exists $ENV{'ProgramFiles(x86)'};
+    @try = () if PRETEND_IT_DOESNT;
+
+    foreach my $try ( @try )
+    {
+        $npp_exe = $try if -x $try;
+        last if defined $npp_exe;
+    }
+
+    warn sprintf "%s\tNPP = %s\n", __PACKAGE__, $npp_exe  if defined $npp_exe;
+    return $npp_exe;
+}
+
+sub determine_bitness {
+    my $bit;
+    warn sprintf "%s\tBITS? \$Config{%s} = %s\n", __PACKAGE__, $_, $Config{$_} for qw/myuname archname ptrsize ivsize/;
+    $bit //= 64 if $Config{archname} =~ /x64/;
+    $bit //= 64 if $Config{ptrsize} >= 8;
+    $bit //= 64 if $Config{ivsize} >= 8;
+    $bit //= 32 if $Config{archname} =~ /x86/;  # this isn't enough to downgrade, so just use this to set initial 32-bit
+    $bit = 32 if $Config{ptrsize} == 4;
+    $bit = 32 if $Config{ivsize} == 4;
+    warn sprintf "%s\tBITS = %d\n", __PACKAGE__, $bit//'<undef>';
+    return $bit;
+}
+
+sub download_zip {
+    my ($bits, $folder) = @_;
+    if( !-w $folder ) {
+        warn sprintf "%s\tZIP: folder '%s' not writeable\n", __PACKAGE__, $folder//'<undef>';
+        return;
+    }
+    warn sprintf "%s\tZIP: folder '%s' ok\n", __PACKAGE__, $folder;
+
+    my %url = (
+        64 => {
+            https => 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.6/npp.7.8.6.bin.x64.zip',
+            http  => 'http://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.6/npp.7.8.6.bin.x64.zip',
+            name  => 'npp.7.8.6.bin.x64.zip',
+        },
+        32 => {
+            https => 'https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.6/npp.7.8.6.bin.zip',
+            http  => 'http://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.6/npp.7.8.6.bin.zip',
+            name  => 'npp.7.8.6.bin.zip',
+        },
+    );
+
+    my $zip = File::Spec->catfile( $folder, $url{$bits}{name});
+    if(-f $zip) {   # already downloaded
+        warn sprintf "%s\tZIP: '%s' already downloaded... moving forward\n", __PACKAGE__, $zip;
+        return $zip;
+    }
+
+    undef $zip;
+    for(qw/https http/) {
+        warn sprintf "%s\tZIP: url = '%s'\n", __PACKAGE__, $url{$bits}{$_};
+        my $ff = File::Fetch->new( uri => $url{$bits}{$_} );
+        next unless $ff;
+        warn sprintf "%s\tZIP: File::Fetch->new(%s) created object\n", __PACKAGE__, $url{$bits}{$_};
+
+        $ff->fetch( to => $folder )
+            and $zip = $ff->output_file()
+            and last
+            or warn sprintf "%s\tZIP: %s\n", __PACKAGE__, $ff->error();
+    }
+    if( defined $zip ) {
+        warn sprintf "%s\tZIP: '%s' downloaded successfully\n", __PACKAGE__, $zip;
+    }
+
+    return $zip;
+}
+
+1;
+__END__
+
+        # 6. Unzip
+        if(defined($downloaded_npp_zip) && -f $downloaded_npp_zip) {
+            use Archive::Extract;
+            my $ae = Archive::Extract->new( archive => $downloaded_npp_zip );
+
+            $downloaded_npp_unzip = catfile( $downloaded_npp_folder, 'notepad++' );
+
+            my $ok = $ae->extract( to => $downloaded_npp_unzip ) or die $ae->error;
+
+            print STDERR "$downloaded_npp_unzip exists\n" if -d $downloaded_npp_unzip;
+        }
+        print STDERR "\n";
+    };
+}
+
+1;
